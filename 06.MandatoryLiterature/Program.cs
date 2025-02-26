6875a767808daae9863eccf919c5aa980b9ee74e
rebelwithoutacause <148477548+rebelwithoutacause@users.noreply.github.com>
2025-02-26 14:28:03 +0200

    Add files via upload

---
+++ 06.MandatoryLiterature/Program.cs
@@ -0,0 +1,8 @@
+﻿int numberOfPages = int.Parse(Console.ReadLine());
+int pagesPerHour = int.Parse(Console.ReadLine());
+int countOfDaysNeeded = int.Parse(Console.ReadLine());
+
+int neededHours = numberOfPages / pagesPerHour;
+int readingHoursPerDay = neededHours / countOfDaysNeeded;
+
+Console.WriteLine(readingHoursPerDay);
